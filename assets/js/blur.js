649c11fcc551b3575f17521e5294e5e4db9c565c
wrnlai <warrenlai@berkeley.edu>
2019-04-29 15:11:44 -0700

    big push

---
+++ assets/js/blur.js
@@ -0,0 +1,28 @@
+// var a = document.getElementById("l")
+// c.onmouseover = unblurLeft
+
+function unblurLeft() {
+	var c = document.getElementById("l")
+	c[1].style.filter = "blur(3px)"
+
+
+}
+
+
+// var b = document.getElementById("r")
+// b.onmouseover = unblurRight
+
+function unblurRight() {
+	var b = document.getElementById("r")
+	b[1].style.filter = "blur(3px)"
+}
+
+
+// var c = document.getElementById("m")
+// c.onmouseover = unblurMiddle
+
+function unblurMiddle() {
+	var c = document.getElementById("m")
+	c[1].style.filter = "blur(3px)"
+}
+
---
+++ assets/js/preview.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+// $("#id-name")
+// $(".class-name")
+// $("div")
+
+	$("#hawaii-txt").hover(function() {
+		$(".preview-img").fadeOut();
+		$("#hawaii").fadeIn();
+		
+
+	})
+
+
+	$("#film-txt").hover(function() {
+		$(".preview-img").fadeOut();
+		$("#film").fadeIn();
+		
+
+	})
+
+
+	$("#asap-txt").hover(function() {
+		$(".preview-img").fadeOut();
+		$("#asap").fadeIn();
+		
+
+	})
+
+
+	$("#sfwoman-txt").hover(function() {
+		$(".preview-img").fadeOut();
+		$("#sfwoman").fadeIn();
+
+
+	})
+
+
+});
